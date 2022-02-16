07ef8e87eb5046e6e7cfa4132828ac4b5c9716ef
M4D1NG3R <67385257+M4D1NG3R@users.noreply.github.com>
2022-02-16 13:01:01 +0100

    Fix for device_info JSON format

--- sagemcom_api/client.py
+++ sagemcom_api/client.py
@@ -362,6 +362,7 @@ class SagemcomClient:
         """Retrieve information about Sagemcom F@st device."""
         try:
             data = await self.get_value_by_xpath("Device/DeviceInfo")
+            return DeviceInfo(**data.get("device_info"))
         except UnknownPathException:
             data = await self.get_values_by_xpaths(
                 {
